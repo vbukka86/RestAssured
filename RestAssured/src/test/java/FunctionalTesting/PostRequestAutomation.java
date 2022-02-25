718127e7b6e1a1af36defd827d958ee849878d86
vbukka <vbukka@localhost>
2022-02-24 18:29:26 -0600

    022422 - Post Request Method

--- RestAssured/src/test/java/FunctionalTesting/PostRequestAutomation.java
+++ RestAssured/src/test/java/FunctionalTesting/PostRequestAutomation.java
@@ -1,5 +1,23 @@
 package FunctionalTesting;
 
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
 public class PostRequestAutomation {
+	
+	@Test
+	public void postRequestAutomation() {
+		
+		RestAssured.baseURI = "https://reqres.in/api/users";
+		
+		String jsonBody = "{\n"
+				+ "    \"name\": \"RA_Test\",\n"
+				+ "    \"job\": \"Automation Engineer\"\n"
+				+ "}";
+		
+		RestAssured.given().body(jsonBody).post().then().log().all().assertThat().statusCode(201);
+		
+	}
 
 }
